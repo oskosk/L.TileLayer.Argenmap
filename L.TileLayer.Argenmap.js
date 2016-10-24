54694b8ac9552af43bf0f280be3df1631c1eb1a2
oskosk <oskosk@gmail.com>
2016-10-24 14:15:58 -0300

    Reemplazar URls de cache servers por la URL del servicio TMS del geowebcache wms.ign.gob.ar/geoserver/gwc

--- L.TileLayer.Argenmap.js
+++ L.TileLayer.Argenmap.js
@@ -1,21 +1,11 @@
 L.TileLayer.Argenmap = L.TileLayer.extend({
-  /** 
+  /**
    * Constant: URL_HASH_FACTOR
    * {Float} Used to hash URL param strings for multi-WMS server selection.
    *         Set to the Golden Ratio per Knuth's recommendation.
    */
   IGN_CACHES : [
-/* estos caches estan desactualizados y no devuelven bien los tiles
-'http://cg.aws.af.cm/tms',
-    'http://robomap-cgastrell.rhcloud.com/tms',
-    'http://sig.ign.gob.ar/tms',
-    'http://190.220.8.216/tms',
-    'http://mapaabierto.aws.af.cm/tms'
-*/
-//estos andan OK (abril 2014)
-'http://igntiles1.ap01.aws.af.cm/tms',
-'http://mapaabierto.aws.af.cm/tms',
-'http://igntiles2.eu01.aws.af.cm/tms'
+    'http://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0'
   ],
   cache: null,
   initialize: function (options) {
@@ -27,7 +17,7 @@ L.TileLayer.Argenmap = L.TileLayer.extend({
     });
     console.log(this.options);
     L.TileLayer.prototype.initialize.call(this, undefined, this.options);
-  },      
+  },
   getTileUrl: function( tilePoint ) {
     var baseURL = '',
       cached=false,
@@ -37,26 +27,26 @@ L.TileLayer.Argenmap = L.TileLayer.extend({
         x: tilePoint.x,
         y: tilePoint.y
       };
-    
+
     baseURL = this.selectURL(tile.x + '' + tile.y),
     cached = this.cache.recuperar(tile.x,tile.y,zoom);
 
     if(cached) {
       return cached;
     }
-    var layerName = "capabaseargenmap";
+    var layerName = "capabaseargenmap@EPSG%3A3857@png";
     console.log('pre');
     if (this.options.layer !== undefined) {
       layerName = this.options.layer;
     }
     var url = baseURL + "/" + layerName + "/" + zoom + "/" + tile.x + '/' + tile.y + ".png";
     this.cache.guardar(tile.x,tile.y,zoom,url);
-    return url;       
+    return url;
   },
   /**
    * Method: selectUrl
    * selectUrl() implements the standard floating-point multiplicative
-   *     hash function described by Knuth, and hashes the contents of the 
+   *     hash function described by Knuth, and hashes the contents of the
    *     given param string into a float between 0 and 1. This float is then
    *     scaled to the size of the provided urls array, and used to select
    *     a URL.
@@ -64,7 +54,7 @@ L.TileLayer.Argenmap = L.TileLayer.extend({
    * Parameters:
    * paramString - {String}
    * urls - {Array(String)}
-   * 
+   *
    * Returns:
    * {String} An entry from the urls array, deterministically selected based
    *          on the paramString.
@@ -81,7 +71,7 @@ L.TileLayer.Argenmap = L.TileLayer.extend({
       product -= Math.floor(product);
     }
     return urls[Math.floor(product * urls.length)];
-  },  
+  },
   /**
    * Clase de cache interna de urls
    */
@@ -99,7 +89,7 @@ L.TileLayer.Argenmap = L.TileLayer.extend({
     {
       var tilecode = x + '-' + y + '-' + z;
 
-      if(this.cache.indexOf(tilecode) != -1) 
+      if(this.cache.indexOf(tilecode) != -1)
       {
         return this.cacheRef[tilecode];
       }
@@ -129,7 +119,7 @@ L.TileLayer.Argenmap = L.TileLayer.extend({
       }
       // console.log('cache set: ' + tilecode + ' guardada, ' + this.cache.length + ' tiles cacheadas');
     };
-            
+
   }
 
 })
